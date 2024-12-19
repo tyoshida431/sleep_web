629db51d4ea7f3d0adbeef0479fe8d84a173e8c1
tetu <tetu@tetuMac-mini.local>
2024-12-19 18:46:44 +0900

    it's delete comments.

--- app/controllers/sleep_controller.rb
+++ app/controllers/sleep_controller.rb
@@ -24,9 +24,6 @@ class SleepController < ApplicationController
       # 月初めと月最後
       first_day=Date.new(year.to_i,month.to_i).strftime("%Y-%m-%d")
       last_day=Date.new(year.to_i,month.to_i,-1).strftime("%Y-%m-%d")
-      #logger.debug first_day
-      #logger.debug last_day
-      #@sleeps=Sleep.all
       sleeps=Sleep.where("date>=? AND date<=?",first_day,last_day)
       # もしレコードが月末まで足りなかったらinsertします。
       # 新しい月の場合も月末まで足りないと判定してinsertします。
@@ -47,25 +44,12 @@ class SleepController < ApplicationController
     begin
       info=request.raw_post
       data=JSON.parse(info)
-      #logger.debug data
-      #data.each{ |value|
-        #logger.debug value
-        #logger.debug value['date']
-        #update_sleeps << value
-        #logger.debug value
-        #update_sleeps << {date: key['date'],wake: key['wake'],bath: key['bath'],bed: key['bath'],sleep_in: key['sleep_in'],sleep: key['sleep'],deep_sleep: key['deep_sleep'],description: key['description']} 
-      #}
       logger.debug "update called"
-      #logger.debug update_sleeps
       ActiveRecord::Base.transaction do
         data.each{ |value|
-          #logger.debug value
-          #logger.debug value['date']
-          #logger.debug value['wake']
           @sleep=Sleep.where("date=?",value['date'])
           @sleep.update(wake: value['wake'],bath: value['bath'],bed: value['bed'],sleep_in: value['sleep_in'],sleep: value['sleep'],deep_sleep: value['deep_sleep'],description: value['description'])
         }
-        #Sleep.update_all(update_sleeps)
       end
     rescue => e
       logger.fatal e
@@ -116,54 +100,20 @@ class SleepController < ApplicationController
       last_day=last_sleep.date
     end
     last_day_for_month=Date.new(year.to_i,month.to_i,-1).strftime("%Y-%m-%d").split("-")
-    #logger.debug last_day
     last_day_fragment=last_day.to_s[9,2].to_i
-    #logger.debug last_day_fragment
     last_day_for_month_day=last_day_for_month[2].to_i
-    #logger.debug last_day_fragment
-    #logger.debug last_day_for_month_day
     insert_sleeps=[]
-    #ret_sleeps=[]
     while last_day_fragment!=last_day_for_month_day do
       last_day_fragment=last_day_fragment+1
-      #logger.debug last_day_fragment
       last_day_fragment_pad=last_day_fragment.to_s
       if last_day_fragment_pad.length==1 then
         last_day_fragment_pad="0"+last_day_fragment_pad
       end
       day=year+"-"+month+"-"+last_day_fragment_pad
-      #logger.debug day
       insert_sleeps << {date: day,wake: 0,bath: 0,bed: 0,sleep_in: "",sleep: "",deep_sleep: "",description: ""}
-    #  sleep=Sleep.new
-    #  sleep.date=day
-    #  sleep.wake=0
-    #  sleep.bath=0
-    #  sleep.bed=0
-    #  sleep.sleep_in=""
-    #  sleep.sleep=""
-    #  sleep.deep_sleep=""
-    #  sleep.description=""
-    #  ret_sleeps.push(sleep)
     end
-    #logger.debug insert_sleeps.length
-    #insert_sleeps.each do |sleep|
-    #  Sleep.create(sleep)
-    #end
-    #insert_sleeps.each do |sleep|
-    #  logger.debug sleep
-    #end
     ActiveRecord::Base.transaction do
       Sleep.insert_all(insert_sleeps)
     end
-    #logger.debug sleeps.length
-    #logger.debug sleeps 
-    #if sleeps.length==0 then
-    #  return ret_sleeps
-    #else
-    #  ret_sleeps.each do |sleep|
-    #    sleeps.push(sleep)
-    #  end
-    #  return sleeps
-    #end
   end
 end
